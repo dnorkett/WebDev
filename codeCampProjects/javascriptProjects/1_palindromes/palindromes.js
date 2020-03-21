fcaba53defad2d31760d8521e1d405440e7ebf74
dnorkett <dnorkett@gmail.com>
2020-03-21 12:10:17 -0500

    reorganized folders

---
+++ codeCampProjects/javascriptProjects/1_palindromes/palindromes.js
@@ -0,0 +1,16 @@
+//Check for a palindrome. Remove all non alpha-numeric characters
+function palindrome(str) {
+    let convertedStr = str.replace(/[^a-zA-Z0-9]/gi, "").toLowerCase();
+    console.log(convertedStr);
+  
+    if (convertedStr.length <= 1) {
+      return true;
+    } else if (convertedStr[0] == convertedStr[convertedStr.length - 1]) {
+      return palindrome(convertedStr.slice(1, convertedStr.length-1));
+    }
+    return false;
+  } 
+  
+  
+  palindrome("1 eye for of 1 eye.");
+  
---
+++ codeCampProjects/javascriptProjects/4_telephoneValidator/telephoneValidator.js
@@ -0,0 +1,14 @@
+//Returns true if the passed string looks like a valid US phone number
+/* 
+555-555-5555
+(555)555-5555
+(555) 555-5555
+555 555 5555
+5555555555
+1 555 555 5555
+*/
+
+function telephoneCheck(str) {
+    var regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
+    return regex.test(str);
+  }
