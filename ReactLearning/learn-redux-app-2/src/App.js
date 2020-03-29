e30d5fab0df1755b7d985c7a5f5ce911e38aa236
dnorkett <dnorkett@gmail.com>
2020-03-29 09:49:55 -0500

    updated redux app

--- ReactLearning/learn-redux-app-2/src/App.js
+++ ReactLearning/learn-redux-app-2/src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {useSelector, useDispatch} from 'react-redux';
+import {increment} from './actions';
+import {decrement} from './actions';
 
 function App() {
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.isLogged);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hello</h1>
+      <p> Counter: {counter} </p>
+      <button onClick={() => dispatch(increment(5))}> + </button>
+      <button onClick={() => dispatch(decrement())}> - </button>
+
+
+      {isLogged ? <p>SECRET INFO</p> : ''}      
     </div>
   );
 }
--- ReactLearning/learn-redux-app-2/src/index.js
+++ ReactLearning/learn-redux-app-2/src/index.js
@@ -3,15 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+import allReducer from './reducers';
+import { Provider } from 'react-redux';  //connects global store to entire app below 
+
+const store = createStore(allReducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>  {/*Put around APP*/}
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
