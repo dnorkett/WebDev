bf2a88ee58d5661a2aead3f8ffa082f91d83eb94
dnorkett <dnorkett@gmail.com>
2020-04-01 12:28:58 -0500

    added markdown previewer project

---
+++ codeCampProjects/reactProjects/2_markdown-previewer/index.js
@@ -0,0 +1,16 @@
+class App extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    
+      render() {
+        return (
+          <div>
+                        
+          </div>
+        );
+      }
+    }
+    
+    ReactDOM.render(<App />,document.getElementById('root'));
+    
