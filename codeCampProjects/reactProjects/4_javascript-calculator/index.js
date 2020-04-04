f8f3d3f20abd482c809c155612ce4115a71fbb82
dnorkett <dnorkett@gmail.com>
2020-04-04 09:40:25 -0500

    added calculator project

---
+++ codeCampProjects/reactProjects/4_javascript-calculator/index.js
@@ -0,0 +1,15 @@
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  
+    render() {
+      return (
+        <div id="calculator">                      
+        </div>
+      );
+    }
+  }
+  
+  ReactDOM.render(<App />,document.getElementById('root'));
+  
